928310d694db9be4e1f7672b62ab6ceb11737163
david :) <david0@umd.edu>
2025-11-25 16:19:25 -0500

    nvim: add lean for class, but disable temporarily, remove lazy-lock since ignored

---
+++ nvim/lua/plugins/lean.lua
@@ -0,0 +1,11 @@
+return {
+  enabled = false,
+  'Julian/lean.nvim',
+  dependencies = {
+    'nvim-lua/plenary.nvim'
+  },
+---@type lean.Config
+  opts = { -- see below for full configuration options
+    mappings = true,
+  }
+}
